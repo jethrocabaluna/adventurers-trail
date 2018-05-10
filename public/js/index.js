ae066b8ce2420ddc4906dd6cad86132bc04b3ad9
jethrocabaluna <jethrocabaluna310@gmail.com>
2018-05-11 02:38:05 +0800

    add security for creating posts

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,18 @@ window.onload = function () {
     }
   }
 
+  function htmlEncode(value) {
+    //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+    //then grab the encoded contents back out.  The div never exists on the page.
+    return $('<div/>').text(value).html();
+  }
+
+  function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  }
+
   // ---------- main scripts ------------- //
   // navbar fade
   if ($('.navbar').length > 0) {
@@ -22,4 +34,14 @@ window.onload = function () {
   $('#edit-button').click(function () {
     $(".image-form").slideToggle(200);
   });
+
+  $('.post-form').submit(function() {
+    $('textarea').val(htmlEncode($('textarea').val()));
+    console.log($('textarea').val());
+    return true;
+  });
+
+  $('#post-content').text(decodeHtml($('#post-content').text()));
+  $('#edit-post-textarea').val(decodeHtml($('#edit-post-textarea').val()));
+
 };
